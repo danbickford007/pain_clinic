942a582e679d15c9a31706c2d91ccb93b775b2ea
Bill <wjones@cblpath.com>
2013-06-29 22:19:28 -0400

    created addresses model and associations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,10 @@
 class Patient < ActiveRecord::Base
 
+has_and_belongs_to_many :addresses
+
 validates :last_name, presence: true, length: { :minimum => 1, :maximum => 50 }
 validates :first_name, presence: true, length: { :minimum => 1, :maximum => 50 }
-validates :email, presence: true, uniqueness: { :case_sensitive => false }
+validates :email, presence: true, uniqueness: { :case_sensitive => false }, :format => {:with =>} 
 validates :gender, presence: true
 validates_length_of :gender, :is =>1
 validates :height, presence: true
