8489508d1704179ff1c9de893078e99e41593b2c
Bill <wjones@cblpath.com>
2013-06-30 18:54:22 -0400

    fixed patient rake task and created the addresses rake task

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,7 @@ class Address < ActiveRecord::Base
 has_and_belongs_to_many :patients
 
 validates :addr_one, :presence => true,:length => {:minimum => 1, :maximum => 75}
-validates :city, :presence => true, :length => :maximum => 50
+validates :city, :presence => true, :length =>{ :maximum => 50 }
 validates :state, :presence => true
 validates_length_of :state, :is => 2
 validates :zip, :presence => true
