919c61fa1720c09c2454572f799d5a3aca255cf7
WilliamTJones <billjones1977@yahoo.com>
2014-06-05 11:42:36 -0400

    testing CI

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,9 +1,9 @@
-require 'test_helper'
+# require 'test_helper'
 
-class DashboardControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+# class DashboardControllerTest < ActionController::TestCase
+#   test "should get index" do
+#     get :index
+#     assert_response :success
+#   end
 
-end
+# end
